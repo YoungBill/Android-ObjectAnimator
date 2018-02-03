c9b7d9e8968e3e3570e2e8ad7ee2616935673b1d
ChenTao <935612713@qq.com>
2018-02-03 23:37:42 +0800

    [feature] 把属性动画应用在Object的属性变化

--- app/src/main/java/com/android/objectanimator/MainActivity.java
+++ app/src/main/java/com/android/objectanimator/MainActivity.java
@@ -1,13 +1,37 @@
 package com.android.objectanimator;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ValueAnimator.AnimatorUpdateListener {
+
+    private Person mPerson;
+    private TextView mPersonTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPersonTv = findViewById(R.id.personTv);
+        mPerson = new Person();
+        mPerson.setName("张三");
+    }
+
+    public void onClick(View view) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(mPerson, "age", 1, 100);
+        objectAnimator.addUpdateListener(this);
+        objectAnimator.setDuration(5000);
+        objectAnimator.start();
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+        int age = (int) animation.getAnimatedValue("age");
+        mPerson.setAge(age);
+        mPersonTv.setText(mPerson.toString() + "\t\t\t" + age + "%");
     }
 }
---
+++ app/src/main/java/com/android/objectanimator/Person.java
@@ -0,0 +1,32 @@
+package com.android.objectanimator;
+
+/**
+ * Created by chentao on 2018/2/3.
+ */
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + " age: " + age;
+    }
+}
